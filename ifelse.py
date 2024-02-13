07dacad56cd56b36c1c184a15334914342f02cbd
mehrthur-teste <mcarthurlemos536@gmail.com>
2024-02-12 23:41:39 -0300

    teste

--- ifelse.py
+++ ifelse.py
@@ -11,3 +11,8 @@ if idade >= 18:
     print("Você é maior de idade.")
 else:
     print("Você é menor de idade.")
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
+    
