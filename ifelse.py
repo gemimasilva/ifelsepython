429c2d2cdc95a8795472a81b26d1ee721267fce9
mehrthur-teste <mcarthurlemos536@gmail.com>
2024-02-12 23:31:18 -0300

    second commit

--- ifelse.py
+++ ifelse.py
@@ -6,3 +6,8 @@ if idade >= 18:
     print("Você é maior de idade.")
 else:
     print("Você é menor de idade.")
+    # Verificando a idade
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
